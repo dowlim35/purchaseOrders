9d6be5bea53d1cdc3f27727eb784722bb870922b
Rachel <rachel.hannigan@liberty-it.co.uk>
2019-07-15 15:25:31 +0100

    Homepage and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {WelcomeComponent} from './components/welcome/welcome.component';
+import {routes} from './routes';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {ContractorComponent} from './components/contractors/contractor.component';
+import {PurchaseOrdersComponent} from './components/purchaseOrders/purchase-orders.component';
+import {TravelComponent} from './components/travel/travel.component';
+import {ExpensesComponent} from './components/expenses/expenses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ContractorComponent,
+    PurchaseOrdersComponent,
+    TravelComponent,
+    ExpensesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes.ts
@@ -0,0 +1,16 @@
+import {Routes} from '@angular/router';
+import {WelcomeComponent} from './components/welcome/welcome.component';
+import {AppComponent} from './app.component';
+import {ContractorComponent} from './components/contractors/contractor.component';
+import {PurchaseOrdersComponent} from './components/purchaseOrders/purchase-orders.component';
+import {TravelComponent} from './components/travel/travel.component';
+import {ExpensesComponent} from './components/expenses/expenses.component';
+
+export const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'contractor', component: ContractorComponent},
+  {path: 'purchaseOrders', component: PurchaseOrdersComponent},
+  {path: 'travel', component: TravelComponent},
+  {path: 'expenses', component: ExpensesComponent}
+];
