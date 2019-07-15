165156c1f200ce45c3cc3fa452e6382506e44928
Michael Dowling <Michael.dowling@liberty-it.co.uk>
2019-07-15 15:59:17 +0100

    Fixed conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,6 @@ import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     ArchiveComponent,
     AppComponent,
     WelcomeComponent,
