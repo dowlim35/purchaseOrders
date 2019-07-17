1b5ac13a0ee6b01f50c62f835f50f41d024854e0
Michael Dowling <Michael.dowling@liberty-it.co.uk>
2019-07-17 16:05:38 +0100

    Added some styling for archive and main pages overall

--- src/app/components/archive/archive.component.ts
+++ src/app/components/archive/archive.component.ts
@@ -11,7 +11,11 @@ export class ArchiveComponent implements OnInit {
   header = ['PO Number', 'Form Type', 'Sub-account', 'Description', 'Date', 'Status'];
   constructor(private dataService: MockService ) { }
   history: History[];
-
+  statusType: any = [
+    {name: 'Approved', value: 'APPROVED'},
+    {name: 'Pending', value: 'PENDING'},
+    {name: 'Denied ', value: 'DENIED'},
+  ]
   ngOnInit() {
     this.getData();
   }
