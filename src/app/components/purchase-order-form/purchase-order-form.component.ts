9ccaed46e59ff670c22a35d7799bb25a8dbe8130
Rachel <rachel.hannigan@liberty-it.co.uk>
2019-07-19 14:38:07 +0100

    Using service to save data

--- src/app/components/purchase-order-form/purchase-order-form.component.ts
+++ src/app/components/purchase-order-form/purchase-order-form.component.ts
@@ -3,13 +3,13 @@ import {PurchaseOrder} from "../../model/purchaseOrder";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {filter} from "rxjs/operators";
 import {NgModel} from "@angular/forms";
-import {PropertiesService} from "../../services/properties.service";
+import {PurchaseOrdersService} from "../../services/purchase-orders.service";
 
 @Component({
   selector: 'app-purchase-orders',
   templateUrl: './purchase-order-form.html',
   styleUrls: ['./purchase-order-form.component.css'],
-  providers: [PropertiesService]
+  providers: [PurchaseOrderFormComponent]
 })
 export class PurchaseOrderFormComponent implements OnInit {
   updateForm: FormGroup;
@@ -17,47 +17,30 @@ export class PurchaseOrderFormComponent implements OnInit {
   quantity: number;
   itemName: string;
   date: Date;
-  currency: symbol;
+  currency: string;
 
-  purchaseorders: PurchaseOrder[];
-  @Input() purchaseOrder: PurchaseOrder;
-  @Output() updateComplete = new EventEmitter<PurchaseOrder>();
 
-  subAccounts: any = [
-    {name: 'A', value: 'A'},
-    {name: 'B', value: 'B'},
-    {name: 'C', value: 'C'},
-    {name: 'D', value: 'D'},
-  ]
   name = "PurchaseOrderUpdateForm";
 
 
-  constructor(private formBuilder: FormBuilder ) {
-    this.updateForm = formBuilder.group({
-      supplierName: ['', Validators.required],
-      itemName: ['', Validators.required],
-      quantity: [' ', Validators.required],
-      currency: [' ', Validators.required],
-      dates : [' ', Validators.required],
-    })
+  constructor(private poService: PurchaseOrdersService) {
+  this.supplierName = poService.getSupplierName();
+  this.itemName = poService.getItemName();
+  this.quantity = poService.getQuantity();
+  this.currency = poService.getCurrency();
+  this.date = poService.getDate();
   }
 
   ngOnInit() {
-    this.updateForm.valueChanges.pipe(filter(values => this.updateForm.valid)).subscribe(values => {
-      console.log('Updating purchase order object via Observable');
-      this.purchaseOrder.supplierName = values.supplierName;
-      this.purchaseOrder.itemName = values.itemName;
-      this.purchaseOrder.quantity = values.quantity;
-      this.purchaseOrder.currency = values.quantity;
-      this.purchaseOrder.dates = values.dates;
 
-    });
-  }
+    };
+
     updatePurchaseOrder(){
-    this.purchaseorders.push({
-      supplierName: this.supplierName,
-      itemName: this.itemName, currency: this.currency, quantity: this.quantity, dates:this.date
-    })
+    this.poService.setSupplierName(this.supplierName);
+      this.poService.setCurrency(this.currency);
+      this.poService.setItemName(this.itemName);
+      this.poService.setQuantity(this.quantity);
+      this.poService.setDate(this.date);
     }
 
 
--- src/app/services/purchase-orders.service.ts
+++ src/app/services/purchase-orders.service.ts
@@ -1,6 +1,66 @@
 import {PurchaseOrder} from '../model/purchaseOrder';
 import {Observable} from 'rxjs';
+import {Injectable} from '@angular/core';
 
-export abstract class PurchaseOrdersService {
-  abstract fetchPurchaseOrders(): Observable<PurchaseOrder[]>;
+@Injectable({
+  providedIn: 'root'})
+
+export class PurchaseOrdersService {
+supplierName: ' ';
+itemName: ' ';
+quantity: number;
+price: number;
+currency:' ';
+date: Date;
+
+getSupplierName()
+{
+  return this.supplierName;
+}
+
+setSupplierName(supplierName)
+{
+  this.supplierName = supplierName;
+}
+
+  getItemName()
+  {
+    return this.itemName;
+  }
+
+  setItemName(itemName)
+  {
+    this.itemName = itemName;
+  }
+
+  getCurrency()
+  {
+    return this.currency;
+  }
+
+  setCurrency(currency)
+  {
+    this.currency = currency;
+  }
+
+  getQuantity()
+  {
+    return this.quantity;
+  }
+
+  setQuantity(quantity)
+  {
+    this.quantity = quantity;
+  }
+
+  getDate()
+  {
+    return this.date;
+  }
+
+  setDate(date)
+  {
+    this.date = date;
+  }
+
 }
