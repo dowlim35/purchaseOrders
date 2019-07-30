c69332566bbfb19158b12972cdbdb86873abc20a
Michael Dowling <Michael.dowling@liberty-it.co.uk>
2019-07-30 10:19:33 +0100

    Testing data

--- src/app/services/history.service.http.ts
+++ src/app/services/history.service.http.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
 import {HistoryService} from './history.service';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {History} from '../model/history';
+import {FormType} from '../../assets/enums/FormType';
+import {ArchiveStatus} from '../../assets/enums/ArchiveStatus';
+import {AccountType} from '../../assets/enums/AccountType';
+import {catchError} from 'rxjs/operators';
+import {error} from 'util';
 
-//@Injectable({
-//  providedIn: 'root'
-//})
 @Injectable()
 export class HistoryServiceHttp extends HistoryService {
-
+  pointer: number;
+  body = {
+    pNo: this.pointer, formType: FormType.TRAINING,
+    subAccount: AccountType.MISC,
+    desc: 'This is dummy Data',
+    date: Date ,
+    status: ArchiveStatus.ACCEPTED
+  };
   constructor(private http: HttpClient) {
     super();
   }
@@ -17,5 +26,23 @@ export class HistoryServiceHttp extends HistoryService {
   fetchHistory(): Observable<History[]> {
     console.log('Data Called From service');
     return this.http.get<History[]>('http://localhost:8080/archive');
+  }
+  putHistory(history: History = {
+    pNo: this.pointer, formType: FormType.TRAINING,
+    subAccount: AccountType.MISC,
+    desc: 'This is dummy Data',
+    date: new Date() ,
+    status: ArchiveStatus.ACCEPTED
+  }): Observable<History> {
+    // this.pointer += 1;
+    const test = JSON.stringify(this.body);
+    console.log('Data stored in service');
+    return this.http.post<History>('http://localhost:8080/archive',
+      history, {headers: new HttpHeaders({
+          'Content-Type' : 'application/json'
+        })
+    })
+      .pipe(catchError(error()));
+
   }
 }
