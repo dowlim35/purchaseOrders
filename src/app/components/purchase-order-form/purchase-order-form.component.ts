fafd7059c782f70bc3d8023e4ab293c96605fe4d
Rachel <rachel.hannigan@liberty-it.co.uk>
2019-07-31 14:41:20 +0100

    Changed from enum type to string and deleted the enum

--- src/app/components/purchase-order-form/purchase-order-form.component.ts
+++ src/app/components/purchase-order-form/purchase-order-form.component.ts
@@ -5,7 +5,7 @@ import {PurchaseOrdersService} from '../../services/purchase-orders.service';
 import {HistoryService} from '../../services/history.service';
 import {History} from '../../model/history';
 import {FormType} from '../../../assets/enums/FormType';
-import {ArchiveStatus} from '../../../assets/enums/ArchiveStatus';
+
 import {AccountType} from '../../../assets/enums/AccountType';
 
 @Component({
@@ -84,7 +84,7 @@ export class PurchaseOrderFormComponent implements OnInit {
     this.results = this.poService.getDetails();
     this.poService.postPurchaseOrders(this.results)
       .subscribe(purchases => this.purchases.push(purchases));
-    this.details = {pNo: 1, formType: this.form, details: this.results, status: ArchiveStatus.PENDING, date: new Date(), desc: (this.supplierName + ' ' + this.itemName + ' ' + this.currency + this.price), subAccount: this.subacc};
+    this.details = {pNo: 1, formType: this.form, details: this.results, status:"PENDING", date: new Date(), desc: (this.supplierName + ' ' + this.itemName + ' ' + this.currency + this.price), subAccount: this.subacc};
     this.archiveService.putHistory(this.details)
       .subscribe(archive => this.archive.push(archive));
   }
